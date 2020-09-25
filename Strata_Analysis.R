5703c8f5bc1c487990d05182e8720e587fb172ea
spaceaphid1 <spaceaphid@gmail.com>
2020-09-25 16:14:50 +0000

    adding strava analysis

---
+++ Strata_Analysis.R
@@ -0,0 +1,5 @@
+#'Strave Analysis
+#'Jackson Anderson
+#'
+#'
+#'Question Addressing: are differences in strava times for a given segment significantly different than if the differences recorded were due to sampling (time recording) error alone?
